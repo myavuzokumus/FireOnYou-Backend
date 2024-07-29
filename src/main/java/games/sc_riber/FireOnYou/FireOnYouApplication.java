689e226b617d835efeec4ad83079e6c9acb36c46
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-07-29 15:00:20 +0300

    ModelMapper added to IOC.

--- src/main/java/games/sc_riber/FireOnYou/FireOnYouApplication.java
+++ src/main/java/games/sc_riber/FireOnYou/FireOnYouApplication.java
@@ -1,13 +1,20 @@
 package games.sc_riber.FireOnYou;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FireOnYouApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FireOnYouApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
