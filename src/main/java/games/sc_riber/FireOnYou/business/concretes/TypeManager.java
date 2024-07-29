9daf3f08b6d5b220edcb4d2400c0ee87539c38ca
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-07-29 15:00:11 +0300

    ModelMapper added.

--- src/main/java/games/sc_riber/FireOnYou/business/concretes/TypeManager.java
+++ src/main/java/games/sc_riber/FireOnYou/business/concretes/TypeManager.java
@@ -3,6 +3,7 @@ package games.sc_riber.FireOnYou.business.concretes;
 import games.sc_riber.FireOnYou.business.abstracts.TypeService;
 import games.sc_riber.FireOnYou.business.requests.CreateVehicleTypeRequest;
 import games.sc_riber.FireOnYou.business.responses.GetAllVehicleTypesResponse;
+import games.sc_riber.FireOnYou.core.utilities.mappers.ModelMapperService;
 import games.sc_riber.FireOnYou.dataAccess.abstracts.VehicleTypeRepository;
 import games.sc_riber.FireOnYou.entities.concretes.VehicleType;
 import lombok.AllArgsConstructor;
@@ -16,16 +17,14 @@ import java.util.List;
 public class TypeManager implements TypeService {
 
     private VehicleTypeRepository vehicleTypeRepository;
+    private ModelMapperService modelMapperService;
 
     @Override
     public List<GetAllVehicleTypesResponse> getAll() {
         // Business rules
         List<VehicleType> types = vehicleTypeRepository.findAll();
-        List<GetAllVehicleTypesResponse> response = new ArrayList<GetAllVehicleTypesResponse>();
-
-        for (VehicleType type : types) {
-            response.add(new GetAllVehicleTypesResponse(type.getId(), type.getVehicleName()));
-        }
+        List<GetAllVehicleTypesResponse> response = types.stream()
+                .map(type -> modelMapperService.forResponse().map(type, GetAllVehicleTypesResponse.class)).toList();
 
         return response;
     }
@@ -33,8 +32,7 @@ public class TypeManager implements TypeService {
     @Override
     public void add(CreateVehicleTypeRequest request) {
         // Business rules
-        VehicleType type = new VehicleType();
-        type.setVehicleName(request.getVehicleName());
+        VehicleType type = this.modelMapperService.forRequest().map(request, VehicleType.class);
         vehicleTypeRepository.save(type);
     }
 
--- src/main/java/games/sc_riber/FireOnYou/webAPI/controllers/TypesController.java
+++ src/main/java/games/sc_riber/FireOnYou/webAPI/controllers/TypesController.java
@@ -6,10 +6,8 @@ import games.sc_riber.FireOnYou.business.responses.GetAllVehicleTypesResponse;
 import games.sc_riber.FireOnYou.entities.concretes.VehicleType;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +18,13 @@ public class TypesController {
 
     private TypeService typeService;
 
-    @GetMapping("/getall") // Bu metodun bir GET request'i karşılayacağını belirtir.
+    @GetMapping() // Bu metodun bir GET request'i karşılayacağını belirtir.
     public List<GetAllVehicleTypesResponse> getAll() {
         return typeService.getAll();
     }
 
-    @PostMapping("/add") // Bu metodun bir GET request'i karşılayacağını belirtir.
+    @PostMapping()
+    @ResponseStatus(code=HttpStatus.CREATED) // Bu metodun bir GET request'i karşılayacağını belirtir.
     public void add(CreateVehicleTypeRequest request) {
         this.typeService.add(request);
     }
