af2b57385ce17bb77e0b9441b6aa3d08bc8d0ea6
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-07-29 17:21:14 +0300

    Unnecessared import is deleted.

--- src/main/java/games/sc_riber/FireOnYou/dataAccess/abstracts/VehicleTypeRepository.java
+++ src/main/java/games/sc_riber/FireOnYou/dataAccess/abstracts/VehicleTypeRepository.java
@@ -3,9 +3,6 @@ package games.sc_riber.FireOnYou.dataAccess.abstracts;
 import games.sc_riber.FireOnYou.entities.concretes.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-
 public interface VehicleTypeRepository extends JpaRepository<VehicleType, Integer>{
 
 }
